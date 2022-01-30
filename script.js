a7448005ac96bfdc626566be411cd0d8bd2ecb77
Ajay Kumar <ajjaystyles@gmail.com>
2022-01-30 23:29:01 +0530

    fivth

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $(".ssc").hide();
+    $(".inter").hide();
+    $(".degree").hide();
+    $(".s").click(function(){
+      $(".ssc").toggle();
+      $(".inter").hide();
+      $(".degree").hide();  
+    })
+    $(".i").click(function(){
+        $(".inter").toggle();
+        $(".ssc").hide();
+        $(".degree").hide();
+      })
+    $(".d").click(function(){
+        $(".degree").toggle();
+        $(".ssc").hide();
+        $(".inter").hide();
+      })
+  });
